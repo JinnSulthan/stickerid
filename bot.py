1c949a8dced38489dab7ce0a5f70618f05aad678
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2024-04-05 22:10:04 +0530

    v2

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import Config
 
-Bot = Client(
+app = Client(
     "StickerIdBot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
@@ -17,7 +17,7 @@ Am Sticker id Finder Bot
 I can Find I'd of an sticker. Just send me a sticker and reply it with /StickerId command i would provide its I'd. . 
 """
     
-@Bot.on_message(filters.private & filters.command(["start"]))
+@app.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
         text=START_TEXT.format(update.from_user.first_name),
@@ -26,16 +26,23 @@ async def start(bot, update):
     )
 START_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Source Code📕', url='https://Github.com/MR-JINN-OF-TG/Stickerid'), 
+        InlineKeyboardButton('Source Code📕', url='https://github.com/JinnSulthan/Stickerid'), 
         InlineKeyboardButton('CHANNEL📕', url=f"https://telegram.me/{Config.CHANNEL}")
         ]]
     )
 
-@Bot.on_message(filters.private & filters.command(["stickerid"]))
+
+
+@app.on_message(filters.command("stickerid") & (filters.private | filters.group))
 async def stickers(bot, message):
-    if message.reply_to_message.sticker:
-       await message.reply(f"Your Requested Sticker's ID is   * `{message.reply_to_message.sticker.file_id}` *", quote=True)
-    else: 
-       await message.reply("Oops !! Not a sticker file")
-   
+    if message.reply_to_message and message.reply_to_message.sticker:
+        await message.reply(
+            f"<b>Your Requested Sticker's ID is:</b> <code>{message.reply_to_message.sticker.file_id}</code>",
+            quote=True
+        )
+    elif message.reply_to_message and not message.reply_to_message.sticker:
+        await message.reply("Oops! The replied message is not a sticker.")
+    else:
+        await message.reply("Are you challenging me!\n reply to a sticker instead.")
+
 Bot.run()
